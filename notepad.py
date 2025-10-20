50e41cda61168d98ca57ed7da9649fbdf6a46584
treasuremargie <treasuremargie@gmail>
2025-10-20 21:39:31 +0300

    mom

--- notepad.py
+++ notepad.py
@@ -1,12 +1,33 @@
 from tkinter.filedialog import *
 import tkinter as tk
- 
 canvas = tk.Tk()
-canvas.geomery("400x600")
+canvas.geometry("400x600")
 canvas.title("Notepad")
-canvas.config(bg = "white")
-
-b1 = Button(canvas, text="Open", bg = "white")
-b1.pack()
-
+canvas.config(bg="white")
+top = tk.Frame(canvas, bg="white")
+top.pack(side=tk.TOP,fill=tk.X,pady=5)
+b1 = tk.Button(top, text="Open", bg="lightgray")
+b1.pack(side=tk.LEFT, padx=2)
+b2 = tk.Button(top, text="Save", bg="lightgray")
+b2.pack(side=tk.LEFT, padx=2)
+b3 = tk.Button(top, text="Clear", bg="lightgray")
+b3.pack(side=tk.LEFT, padx=2)
+b4 = tk.Button(top, text="Exit", bg="lightgray")
+b4.pack(side=tk.LEFT, padx=2)
+entry = tk.Text(canvas,wrap=tk.WORD,bg=)
 canvas.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
